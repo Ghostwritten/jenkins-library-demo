dda9e5f8a2b203542d2d0e1e2aa6fbd70b37f1a2
ghostwritten <1zoxun1@gmail.com>
2022-12-15 13:40:39 +0800

    fix helloWorldExternal.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"
+  def scriptcontents = libraryResource "com/ghostwritten/scripts/linux/${config.name}"
   writeFile file: "${config.name}", text: scriptcontents
   sh "chmod a+x ./${config.name}"
 }
